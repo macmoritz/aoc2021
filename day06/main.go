f1a8458938ed70832754f29528f227215719b303
moritz <m.tratar@senbax.computer>
2022-01-28 09:22:35 +0100

    less code in day06 go

--- day06/main.go
+++ day06/main.go
@@ -10,15 +10,7 @@ func simulate(fishs [9]uint64, days int) [9]uint64 {
 	var temp uint64;
 	for day := 0; day < days; day++ {
 		temp = fishs[0];
-		fishs[0] = fishs[1];
-		fishs[1] = fishs[2];
-		fishs[2] = fishs[3];
-		fishs[3] = fishs[4];
-		fishs[4] = fishs[5];
-		fishs[5] = fishs[6];
-		fishs[6] = fishs[7] + temp;
-		fishs[7] = fishs[8];
-		fishs[8] = temp;
+		fishs[0], fishs[1], fishs[2], fishs[3], fishs[4], fishs[5], fishs[6], fishs[7], fishs[8] = fishs[1], fishs[2], fishs[3], fishs[4], fishs[5], fishs[6], fishs[7] + temp, fishs[8], temp;
 	}
 	return fishs
 }
@@ -36,8 +28,7 @@ func main() {
 	}
 	var init = strings.Trim(strings.ReplaceAll(string(data), ",", ""), "\n")
 
-	var fishs1 = [9]uint64{0, 0, 0, 0, 0, 0, 0, 0, 0}
-	var fishs2 = [9]uint64{0, 0, 0, 0, 0, 0, 0, 0, 0}
+	var fishs1, fishs2 = [9]uint64{0, 0, 0, 0, 0, 0, 0, 0, 0}, [9]uint64{0, 0, 0, 0, 0, 0, 0, 0, 0}
 	var part1, part2 uint64 = 0, 0
 
 	for _, fishs := range init {
@@ -45,8 +36,7 @@ func main() {
 		fishs2[int(fishs) - int('0')] += 1;
 	}
 
-	fishs1 = simulate(fishs1, 80)
-	fishs2 = simulate(fishs2, 256)
+	fishs1, fishs2 = simulate(fishs1, 80), simulate(fishs2, 256)
 	// fishs2 = simulate(fishs2, 9999999999)
 
 	for i := 0; i < 9; i++ {
@@ -54,6 +44,5 @@ func main() {
 		part2 += fishs2[i]
 	}
 
-	fmt.Println("part 1:", part1)
-	fmt.Println("part 2:", part2)
+	fmt.Println("part 1:", part1, "\npart2:", part2)
 }
